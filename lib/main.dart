a1679d03004d8a6b324a2c5a52379fddc66c8dc3
unknown <harunerenozkaya@gmail.com>
2020-07-03 22:07:19 +0300

    Baslangic

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ToDoApp());
+}
+
+class ToDoApp extends StatefulWidget {
+  @override
+  _ToDoAppState createState() => _ToDoAppState();
+}
+
+class _ToDoAppState extends State<ToDoApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
